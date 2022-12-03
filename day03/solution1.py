8d008953254e0e701337dbafb2c9bc28ad3cca01
Rob Harman <61101581+robharman@users.noreply.github.com>
2022-12-03 03:37:02 -0500

    Simplify

--- day03/solution1.py
+++ day03/solution1.py
@@ -1,14 +1,13 @@
 def prioritizeSacks(sacks: str) -> int:
     total = 0
-    priorities = []
 
     for sack in sacks.split():
-        for i in sack[0:int((len(sack) / 2))]:
-            if i in sack[(int((len(sack) / 2))):len(sack)]:
+        for i in sack[0:(len(sack) // 2)]:
+            if i in sack[((len(sack) // 2)):]:
                 if ord(i) >= 97:
-                    priorities.append(ord(i) - 96)
+                    total += (ord(i) - 96)
                 else:
-                    priorities.append(ord(i) - 38)
+                    total += (ord(i) - 38)
                 break
 
-    return sum(priorities)
+    return total
--- day03/solution2.py
+++ day03/solution2.py
@@ -1,17 +1,15 @@
 def returnBadgetotal(sacks: str) -> int:
-    sackIndex = 0
+    total = sackIndex = 0
     sackList = sacks.split()
-    priorities = []
 
     while sackIndex < len(sackList):
         for c in sackList[sackIndex]:
             if c in sackList[sackIndex + 1] and c in sackList[sackIndex + 2]:
                 if ord(c) >= 97:
-                    priorities.append(ord(c) - 96)
+                    total += (ord(c) - 96)
                 else:
-                    priorities.append(ord(c) - 38)
+                    total += (ord(c) - 38)
                 break
-
         sackIndex += 3
 
-    return sum(priorities)
+    return total
